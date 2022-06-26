12b49b925e4da6166f4480681d749772ea36e909
Sayeedh784 <sayeedh74@gmail.com>
2022-06-26 20:05:14 +0600

    updated

--- maritalstatus/settings.py
+++ maritalstatus/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1','maritalstatus.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
